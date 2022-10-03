fb6790db7f7641f3955c3910feeb99ddf4a89782
HayHay <dev@hayhay.dev>
2022-10-03 15:07:50 -0400

    lowercase grep

--- grep/grep.c
+++ grep/grep.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) return 1;
 
-    char *compareTo = argv[1];
+    char *compareTo = argv[1];\
+    int j = 0;
+    while (compareTo[j] != '\0') {
+        compareTo[j] = tolower(compareTo[j]);
+        j++;
+    }
     char stdInWord[4096];
     while (fgets(stdInWord, sizeof(stdInWord), stdin) != NULL) {
-        if (strstr(stdInWord, compareTo) != NULL) {
+        char stdInWordCopy[4096];
+        int i = 0;
+        while (stdInWord[i] != '\0') {
+            stdInWordCopy[i] = tolower(stdInWord[i]);
+            i++;
+        }
+        if (strstr(stdInWordCopy, compareTo) != NULL) {
             printf("%s", stdInWord);
         }
     }
