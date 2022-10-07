736e9795ead3ac848bcb76fabb0dff4ee6f9d626
HayHay <dev@hayhay.dev>
2022-10-07 04:24:48 -0400

    Grep for case sensitivity

--- grep/grep.c
+++ grep/grep.c
@@ -2,22 +2,30 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) return 1;
+int main(int argc, char **argv) {
+    if (argc > 3) return 1;
 
-    char *compareTo = argv[1];\
-    int j = 0;
-    while (compareTo[j] != '\0') {
-        compareTo[j] = tolower(compareTo[j]);
-        j++;
+    char *compareTo;
+    if (argc == 1) {
+        printf("Non standard usage.");
+        return 1;
+    } else if (argc == 2) {
+        compareTo = argv[1];
+    } else {
+        compareTo = argv[2];
     }
-    char stdInWord[4096];
-    while (fgets(stdInWord, sizeof(stdInWord), stdin) != NULL) {
-        char stdInWordCopy[4096];
-        int i = 0;
-        while (stdInWord[i] != '\0') {
-            stdInWordCopy[i] = tolower(stdInWord[i]);
-            i++;
+    
+    size_t size = 0;
+    char *stdInWord = NULL;
+    
+    while (getline(&stdInWord, &size, stdin) != -1) {
+        char *stdInWordCopy = strdup(stdInWord);
+        if (strcmp(argv[1], "-i") == 0) {
+            int i = 0;
+            while (stdInWordCopy[i] != '\0') {
+                stdInWordCopy[i] = tolower(stdInWordCopy[i]);
+                i++;
+            }
         }
         if (strstr(stdInWordCopy, compareTo) != NULL) {
             printf("%s", stdInWord);
